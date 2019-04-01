6a235f7d0b01a723d69494c8c8e0b2b60b58a9e2
smilepyt <307080368@qq.com>
2019-04-01 21:20:30 +0800

    学习react

---
+++ src/components/Records.js
@@ -0,0 +1,140 @@
+import React, { Component } from 'react';
+import Record from './Record';
+import * as RecordsAPI from '../utils/RecordsAPI'
+import RecordForm from './RecordForm'
+import AmountBox from './AmountBox'
+
+class Records extends Component {
+  constructor() {
+    super();
+    this.state = {
+      error: null,
+      isLoaded: false,
+      records: [
+      ]
+    }
+  }
+
+  componentDidMount() {
+    RecordsAPI.getAll().then(
+      response => this.setState({
+        records: response.data,
+        isLoaded: true
+      })
+    ).catch(
+      error => this.setState({
+        isLoaded: false,
+        error
+      })
+    )
+  }
+
+  addRecord(record) {
+    this.setState({
+      error: null,
+      isLoaded: true,
+      records: [
+        ...this.state.records,
+        record
+      ]
+    })
+  }
+
+  updateRecord(record,data) {
+    const recordIndex = this.state.records.indexOf(record);
+    const newRecords = this.state.records.map((item,index) => {
+      if (index !== recordIndex) {
+        return item;        
+      }
+      return {
+        ...item,
+        ...data
+      }
+    });
+    this.setState({
+      records: newRecords
+    });
+  }
+  
+  deleteRecord(record) {
+    const recordIndex = this.state.records.indexOf(record);
+    const newRecords = this.state.records.filter( (item, index) => index !== recordIndex);
+    this.setState({
+      records: newRecords
+    });
+  }
+
+  credits() {
+    let credits = this.state.records.filter((record) => {
+      return record.amount >=0
+    })
+    
+    return credits.reduce((prev,curr) => {
+      return prev + Number.parseInt(curr.amount,0);
+    },0)
+  }
+
+  debits() {
+    let debits = this.state.records.filter((record) => {
+      return record.amount <0
+    })
+    
+    return debits.reduce((prev,curr) => {
+      return prev + Number.parseInt(curr.amount,0);
+    },0)
+  }
+
+  balance() {
+    return this.credits() + this.debits();
+  }
+
+  render() {
+    const {error,isLoaded,records} = this.state;
+    let recordsComponent;
+
+    if (error) {
+      recordsComponent = <div>Error: {error.message}</div>
+    }
+    else if (!isLoaded) {
+      recordsComponent = <div>Loading...</div>
+    }
+    else {
+      recordsComponent = (
+        <div>
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Title</th>
+              <th>Amount</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {records.map((record) => 
+            <Record 
+            record={record} 
+            key={record.id} 
+            handleEditRecord={this.updateRecord.bind(this)}
+            handleDeleteRecord={this.deleteRecord.bind(this)}/>)}
+          </tbody>
+        </table>
+      </div>
+      )
+    }
+    return (
+      <div>
+        <h2>Records</h2>
+        <div className="row mb-3">
+          <AmountBox text="Credits" type="success" amount={this.credits()}/>
+          <AmountBox text="Debits" type="danger" amount={this.debits()}/>
+          <AmountBox text="Balance" type="info" amount={this.balance()}/>
+        </div>
+        <RecordForm handleNewRecord={this.addRecord.bind(this)}/>
+        {recordsComponent}
+      </div>
+    )
+  }
+}
+
+export default Records;
